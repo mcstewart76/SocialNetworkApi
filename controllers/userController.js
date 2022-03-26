2bb560c92e361a964792b99aa8a7d69a622e1dfe
mcstewart76 <mcstewart76@gmail.com>
2022-03-26 18:10:21 -0400

    Fixed issue with delete friend syntax

--- controllers/userController.js
+++ controllers/userController.js
@@ -93,7 +93,7 @@ addFriend(req, res) {
   deleteFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $pull: { friends: { friendId: req.params.friendId } } },
+      { $pull: { friends:  req.params.friendId } },
       { runValidators: true, new: true }
     )
       .then((user) =>
