92c050f0a769e2357f4b34aaa84803ac39ad6b82
mcstewart76 <mcstewart76@gmail.com>
2022-03-26 15:47:55 -0400

    updated reaction schema to be in thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,36 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
 
+
+//reaction schema
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        
+      },
+    
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
 
 // Schema to create Thought model
 const thoughtSchema = new Schema(
--- models/User.js
+++ models/User.js
@@ -34,6 +34,10 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
+});
+
 const User = model('user', userSchema);
 
 module.exports = User;
